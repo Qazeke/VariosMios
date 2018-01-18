127da959e3cb42397bc38df0ad04948cac8be245
Qazeke <sergiozgz18@gmail.com>
2018-01-18 13:35:21 +0100

    borrando

--- j8.sh
+++
@@ -1,3 +0,0 @@
-sudo update-java-alternatives -s java-8-oracle
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
-export PATH=$PATH:$JAVA_HOME
--- j9.sh
+++
@@ -1,3 +0,0 @@
-sudo update-java-alternatives -s java-9-oracle
-export JAVA_HOME=/usr/lib/jvm/java-9-oracle/
-export PATH=$PATH:$JAVA_HOME
